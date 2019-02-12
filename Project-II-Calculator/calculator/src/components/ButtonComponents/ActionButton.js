85fdce10b6c9eefb197bfac5cf4861304d43e52b
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-02-12 17:00:39 -0500

    can receive input

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+
+const ActionButton = (props) => {
+    return <button onClick = {() => props.onClick(props.op)}>{props.op}</button>   
+}
+
+
+export default ActionButton;
